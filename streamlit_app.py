1724e1510d867ea935674f92af2a75368e3b9a1d
pradhanrahul2511 <pradhanrahul2511@gmail.com>
2024-05-28 12:32:07 +0530

    Update streamlit_app.py
    ADDED DF

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,9 +50,13 @@ if ingredients_list:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!,'+name_on_order, icon="✅")
 
+#new section to display fuityvice nutrition options
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width =TRUE)
+
+
 
 
 
