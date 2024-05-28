ca8a3fc4309d15ba7278a02a10188c044a35c3ca
pradhanrahul2511 <pradhanrahul2511@gmail.com>
2024-05-28 12:40:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 
 
 from snowflake.snowpark.functions import col
@@ -37,6 +38,9 @@ if ingredients_list:
 
   for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+      #st.text(fruityvice_response.json())
+      fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
   #st.write(ingredients_string)
 
@@ -51,8 +55,6 @@ if ingredients_list:
     st.success('Your Smoothie is ordered!,'+name_on_order, icon="✅")
 
 #new section to display fuityvice nutrition options
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
+
+
 
