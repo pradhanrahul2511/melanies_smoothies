e0cec9f6f9f77106e103eabac3583d2450bfa335
pradhanrahul2511 <pradhanrahul2511@gmail.com>
2024-05-28 12:37:10 +0530

    Update streamlit_app.py
    EARLIER

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,12 +53,6 @@ if ingredients_list:
 #new section to display fuityvice nutrition options
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
+st.text(fruityvice_response.json())
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
 
-
-
-
-
-
-      
